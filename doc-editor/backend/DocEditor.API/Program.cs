e40ccaad6f0938fe0a09c542e7296d43a1885d3b
Ai_architect <kishorpv1029@gmail.com>
2026-06-09 20:23:02 +0530

    deploy

--- doc-editor/backend/DocEditor.API/Program.cs
+++ doc-editor/backend/DocEditor.API/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
